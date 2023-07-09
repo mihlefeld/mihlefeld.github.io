d85559abbc59bc34a71d936cb25045bb2ade15e3
Malte Ihlefeld <malte@famihlefeld.de>
2023-07-09 12:08:04 +0200

    added face completion and face quality

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ function set_columns() {
     var first_project = document.getElementById("first_project");
     var child_w = first_project.offsetWidth * 1.1;
     var num = ~~(w / child_w);
-    console.log(w, child_w);
+    num = Math.min(num, 3);
+    // console.log(min(num, 3), num, 3);
     project_list.style.setProperty("--num_cols", num);
 }
 
